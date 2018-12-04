9bd79364cd2c8c888a922f0d50245eee47f6a0de
parthvaghasiya13 <35321448+parthvaghasiya13@users.noreply.github.com>
2018-12-04 17:33:32 -0600

    Add files via upload

--- models/order.js
+++ models/order.js
@@ -1,10 +1,9 @@
 var mongoose = require('mongoose');
 var orderSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  director: { type: String, required: true },
-  cast: {type: String, required: true },
   showDate: {type: Date, required: false},
-  showTime: {type: String, required: true}
+  showTime: {type: String, required: true},
+  numberTickets: {type: Number, required: true}
 });
 
 var Order = mongoose.model('Order', orderSchema);
